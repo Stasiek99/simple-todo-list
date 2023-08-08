3d63c745b186a629b80013fe02276dd54ab7eae8
Stasiek99 <stasiekpilich@gmail.com>
2023-08-08 21:22:24 +0200

    small refactor

--- src/app/user/interfaces/user.interface.ts
+++ src/app/user/interfaces/user.interface.ts
@@ -1,5 +1,5 @@
 export interface UserInterface {
-  id: number,
+  id: string,
   name: string,
   login: string,
   password: string,
--- src/app/user/user-local-storage.service.ts
+++ src/app/user/user-local-storage.service.ts
@@ -8,6 +8,8 @@ import { UserInterface } from "./interfaces/user.interface";
 
 export class UserLocalStorageService {
   private readonly userStorageItemKey = "users_list";
+  private selectedUserid: string | null = null;
+  selectedUserId: string = "";
 
   private getUserList(): UserInterface[] {
     const objectToParse = window.localStorage.getItem(this.userStorageItemKey);
@@ -20,9 +22,24 @@ export class UserLocalStorageService {
 
   addUser(user: UserInterface): void {
     const userList = this.getUserList();
-    user.id = userList.length;
+    user.id = userList.length.toString();
     userList.push(user);
+    this.selectedUserId = (userList.length -1).toString();
     this.setUserList(userList);
+  }
+
+  getUser(userId: string): UserInterface | null {
+    const userList = this.getUserList();
+    const user = userList.find(user => user.id === userId);
+    return user || null;
+  }
+
+  setSelectedUser(userId: string): void {
+    this.selectedUserid = userId;
+  }
+
+  getSelectedUser(): UserInterface | null {
+    return this.selectedUserid ? this.getUser(this.selectedUserid) : null
   }
 
   getUsers(): UserInterface[] {
