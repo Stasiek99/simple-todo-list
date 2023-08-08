f068b1a9a165b3faadc9c1709b2815662b7ad999
Stasiek99 <stasiekpilich@gmail.com>
2023-08-08 15:01:06 +0200

    signup-snackbar.component added

---
+++ src/app/signup/components/signup-snackbar/signup-snackbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-snackbar',
+  templateUrl: './signup-snackbar.component.html',
+  styleUrls: ['./signup-snackbar.component.scss']
+})
+export class SignupSnackbarComponent {}
--- src/app/signup/components/signup/signup.component.ts
+++ src/app/signup/components/signup/signup.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { MatDialog } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 import { SignupDialogComponent } from "../signup-dialog/signup-dialog.component";
+import { SignupSnackbarComponent } from "../signup-snackbar/signup-snackbar.component";
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss'],
-
+  styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
   newUserForm: FormGroup = new FormGroup({
@@ -20,7 +21,8 @@ export class SignupComponent {
     "email": new FormControl(null, Validators.required)
   });
 
-  constructor(private router: Router, private dialog: MatDialog) {}
+  durationInSeconds: number = 5;
+  constructor(private router: Router, private dialog: MatDialog, private snackBar: MatSnackBar) {}
 
   openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
     let dialogRef = this.dialog.open(SignupDialogComponent, {
@@ -31,8 +33,15 @@ export class SignupComponent {
     });
     dialogRef.afterClosed().subscribe(result => {
       if(result) {
+        this.openSnackBar();
         this.redirectToTodoList();
       }
+    });
+  }
+
+  openSnackBar(): void {
+    this.snackBar.openFromComponent(SignupSnackbarComponent, {
+      duration: this.durationInSeconds * 1000
     });
   }
 
