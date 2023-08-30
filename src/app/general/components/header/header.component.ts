8ce038df53cf8c2357efe8d4074ea19254e6083d
Stasiek99 <stasiekpilich@gmail.com>
2023-08-30 13:03:02 +0200

    small refactor

--- src/app/general/components/header/header.component.ts
+++ src/app/general/components/header/header.component.ts
@@ -1,12 +1,22 @@
-import {Component, Output, EventEmitter} from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+
+import { CurrentUserService } from "../../../signup/services/current-user.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   @Output() menuIconClicked = new EventEmitter<void>();
+  userId: string | undefined = undefined;
+
+  constructor(private currentUserService: CurrentUserService) {}
+
+  ngOnInit(): void {
+    this.userId = this.currentUserService.getCurrentUser()?.id;
+  }
+
 
   onClickMenuIcon(): void {
     this.menuIconClicked.emit();
