2f1fe79add4707eafc9f8a75133daa6b5478956f
Stasiek99 <stasiekpilich@gmail.com>
2023-09-15 20:29:26 +0200

    lazy loading added

---
+++ src/app/todo-list/todo-list-routing.module.ts
@@ -0,0 +1,33 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+
+import {AuthGuard} from "../auth/guards/auth.guard";
+import {TodoListComponent} from "./todo-list/todo-list.component";
+import {TodoListElementComponent} from "./todo-list-element/todo-list-element.component";
+
+const routes: Routes = [
+  {
+    path: "", canActivate: [AuthGuard],
+    children: [
+      {
+        path: "",
+        component: TodoListComponent
+      },
+      {
+        path: "todo/:todoId",
+        component: TodoListElementComponent
+      }
+    ]
+  },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class TodoListRoutingModule {
+}
