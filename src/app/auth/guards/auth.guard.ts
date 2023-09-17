365551758b99376da8008c48b754bf499107bf56
Stasiek99 <stasiekpilich@gmail.com>
2023-09-17 20:15:44 +0200

    small refactor

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,15 +1,16 @@
-import { Injectable } from "@angular/core";
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from "@angular/router";
-import { Observable } from "rxjs";
+import {Injectable} from "@angular/core";
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from "@angular/router";
+import {Observable} from "rxjs";
 
-import { CurrentUserService } from "../../signup/services/current-user.service";
-import { UserInterface } from "../../user/interfaces/user.interface";
+import {CurrentUserService} from "../../signup/services/current-user.service";
+import {UserInterface} from "../../user/interfaces/user.interface";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthGuard implements CanActivate {
-  constructor(private currentUserService: CurrentUserService, private router: Router) {}
+  constructor(private currentUserService: CurrentUserService, private router: Router) {
+  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     const currentUser: UserInterface | null = this.currentUserService.getCurrentUser();
