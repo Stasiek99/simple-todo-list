e4760b0f85ec2431c2d6acae1978040e074a46a2
Stasiek99 <stasiekpilich@gmail.com>
2023-09-19 17:37:53 +0200

    app-tree refactored

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 
-import {HomeComponent} from "./general/components/home/home.component";
-import {PageNotFoundComponent} from "./general/components/page-not-found/page-not-found.component";
-import {AdminComponent} from "./auth/components/admin/admin.component";
-import {AdminGuard} from "./auth/guards/admin.guard";
-import {UnapprovedUserComponent} from "./signup/components/unapproved-user/unapproved-user.component";
+import {HomeComponent} from "./todo-list/general/components/home/home.component";
+import {PageNotFoundComponent} from "./todo-list/general/components/page-not-found/page-not-found.component";
+import {AdminComponent} from "./todo-list/auth/components/admin/admin.component";
+import {AdminGuard} from "./todo-list/auth/guards/admin.guard";
+import {UnapprovedUserComponent} from "./todo-list/signup/components/unapproved-user/unapproved-user.component";
 
 export const routeConfig: Routes = [
   {path: "", redirectTo: "home", pathMatch: "full"},
@@ -14,15 +14,15 @@ export const routeConfig: Routes = [
   {path: "unapproved", component: UnapprovedUserComponent},
   {
     path: "todolist/:userId",
-    loadChildren: () => import("./todo-list/todo-list-routing.module").then((m) => m.TodoListRoutingModule)
+    loadChildren: () => import("./todo-list/todo-list/todo-list-routing.module").then((m) => m.TodoListRoutingModule)
   },
   {
     path: "users",
-    loadChildren: () => import("./user/user-routing.module").then((m) => m.UserRoutingModule)
+    loadChildren: () => import("./todo-list/user/user-routing.module").then((m) => m.UserRoutingModule)
   },
   {
     path: "auth",
-    loadChildren: () => import("./auth/auth-routing.module").then((m) => m.AuthRoutingModule)
+    loadChildren: () => import("./todo-list/auth/auth-routing.module").then((m) => m.AuthRoutingModule)
   },
   {
     path: "admin", canActivate: [AdminGuard], component: AdminComponent
